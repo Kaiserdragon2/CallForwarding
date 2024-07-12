4950805b3a11682bde56ad05c88d6b8bc5586936
Bastian <8929967+Kaiserdragon2@users.noreply.github.com>
2024-07-12 23:07:52 +0200

    Optimize Phone state service

--- app/src/main/java/de/kaiserdragon/callforwardingstatus/PhoneStateService.java
+++ app/src/main/java/de/kaiserdragon/callforwardingstatus/PhoneStateService.java
@@ -9,37 +9,32 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
-import android.appwidget.AppWidgetManager;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
-import android.telephony.TelephonyCallback;
 import android.telephony.TelephonyManager;
 import android.util.Log;
-
-import androidx.annotation.RequiresApi;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-import kotlin.Deprecated;
-import kotlin.Suppress;
+import de.kaiserdragon.callforwardingstatus.helper.CallForwardingListener;
 
 public class PhoneStateService extends Service {
     private static final String CHANNEL_ID = "CallForwardingServiceID";
     public static boolean currentState;
     Context appcontext;
     static final String TAG = "Service";
-    private final Executor executor = Executors.newSingleThreadExecutor();
+
     // Define NOTIFICATION_ID as a constant
     private static final int NOTIFICATION_ID = 1;
-    private MyCallForwardingListener callForwardingListener;
+    private CallForwardingListener callForwardingListener;
 
 
     /** @noinspection deprecation*/
@@ -48,19 +43,11 @@ public class PhoneStateService extends Service {
         @Override
         public void onCallForwardingIndicatorChanged(boolean cfi) {
             if (DEBUG)Log.i(TAG, "onCallForwardingIndicatorChanged  CFI  Old=" + cfi);
-            // Get the current state of unconditional call forwarding
-            currentState = cfi;
             // Create an Intent with the android.appwidget.action.APPWIDGET_UPDATE action
             Intent intent = new Intent(appcontext, ForwardingStatusWidget.class);
             intent.setAction("de.kaiserdragon.callforwardingstatus.APPWIDGET_UPDATE_CFI");
-            //Toast.makeText(context, "OLD", Toast.LENGTH_SHORT).show();
-            // Add the app widget IDs as an extra
-            int[] ids = AppWidgetManager.getInstance(getApplication()).getAppWidgetIds(new ComponentName(getApplication(), ForwardingStatusWidget.class));
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
-
             // Add the CFI value as an extra
-            intent.putExtra("cfi", currentState);
-
+            intent.putExtra("cfi", cfi);
             // Send the broadcast
             sendBroadcast(intent);
 
@@ -124,13 +111,13 @@ public class PhoneStateService extends Service {
         TelephonyManager telephonyManager = getSystemService(TelephonyManager.class); // Use class reference for type safety
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             if (callForwardingListener == null) { // Check if already registered
-                callForwardingListener = new MyCallForwardingListener();
-                telephonyManager.registerTelephonyCallback(executor, callForwardingListener);
+                callForwardingListener = new CallForwardingListener(appcontext);
+                telephonyManager.registerTelephonyCallback(Executors. newSingleThreadExecutor(), callForwardingListener);
                 if (DEBUG) Log.i(TAG, "Registered TelephonyCallback");
             }
         } else {
             telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_FORWARDING_INDICATOR);
-            if (DEBUG) Log.i(TAG, "Registered PhoneStateListener");
+            //if (DEBUG) Log.i(TAG, "Registered PhoneStateListener");
         }
     }
 
@@ -149,35 +136,10 @@ public class PhoneStateService extends Service {
         super.onDestroy();
     }
 
+    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        // Return null as this service is not bound to any activity
         return null;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.S)
-    public class MyCallForwardingListener extends TelephonyCallback implements TelephonyCallback.CallForwardingIndicatorListener {
-        @Override
-        public void onCallForwardingIndicatorChanged(boolean cfi) {
-                // Handle the call forwarding state change here
-                if (DEBUG) Log.i(TAG, "onCallForwardingIndicatorChanged  CFI New=" + cfi);
-                // Get the current state of unconditional call forwarding
-                //Toast.makeText(context, "New", Toast.LENGTH_SHORT).show();
-                currentState = cfi;
-                // Create an Intent with the android.appwidget.action.APPWIDGET_UPDATE action
-                Intent intent = new Intent(appcontext, ForwardingStatusWidget.class);
-                intent.setAction("de.kaiserdragon.callforwardingstatus.APPWIDGET_UPDATE_CFI");
-
-                // Add the app widget IDs as an extra
-                int[] ids = AppWidgetManager.getInstance(getApplication()).getAppWidgetIds(new ComponentName(getApplication(), ForwardingStatusWidget.class));
-                intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
-
-                // Add the CFI value as an extra
-                intent.putExtra("cfi", currentState);
-
-                // Send the broadcast
-                sendBroadcast(intent);
-
-        }
-    }
 }
