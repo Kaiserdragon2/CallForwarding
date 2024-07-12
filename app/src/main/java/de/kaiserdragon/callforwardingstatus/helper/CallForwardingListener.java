12407e792a9b45d6956969e0afa7ce8a59fab3e8
Bastian <8929967+Kaiserdragon2@users.noreply.github.com>
2024-07-12 23:06:44 +0200

    Create CallForwardingListener.java

---
+++ app/src/main/java/de/kaiserdragon/callforwardingstatus/helper/CallForwardingListener.java
@@ -0,0 +1,49 @@
+package de.kaiserdragon.callforwardingstatus.helper;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.telephony.TelephonyCallback;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import java.lang.ref.WeakReference;
+
+import de.kaiserdragon.callforwardingstatus.BuildConfig;
+import de.kaiserdragon.callforwardingstatus.ForwardingStatusWidget;
+import de.kaiserdragon.callforwardingstatus.PhoneStateService;
+
+@RequiresApi(api = Build.VERSION_CODES.S)
+public class CallForwardingListener extends TelephonyCallback implements TelephonyCallback.CallForwardingIndicatorListener {
+
+    private static final String TAG = "CallForwardingListener";
+    private static WeakReference<Context> contextRef;
+
+    public CallForwardingListener(@NonNull Context context) {
+        contextRef = new WeakReference<>(context);
+    }
+
+    @Override
+    public void onCallForwardingIndicatorChanged(boolean callForwardingIndicator) {
+
+            if (BuildConfig.DEBUG) {
+                Log.i(TAG, "onCallForwardingIndicatorChanged - New CFI (different from last): " + callForwardingIndicator);
+
+                PhoneStateService.currentState = callForwardingIndicator;
+                sendWidgetUpdateBroadcast(callForwardingIndicator);
+            }
+
+    }
+
+    private void sendWidgetUpdateBroadcast(boolean callForwardingIndicator) {
+        Context context = contextRef.get();
+        Intent intent = new Intent(context, ForwardingStatusWidget.class);
+        intent.setAction("de.kaiserdragon.callforwardingstatus.APPWIDGET_UPDATE_CFI");
+        // Add the CFI value as an extra
+        intent.putExtra("cfi", callForwardingIndicator);
+        // Send the broadcast
+        context.sendBroadcast(intent);
+    }
+}
